3dd64c641c4670c0bc602354c6ad800820d7130c
3RBNK <dawidenkokirill@gmail.com>
2024-02-24 15:35:22 +0300

    add multiply matrix and square of matrix function

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -16,4 +16,8 @@ void sort_rows_by_max_element(matrix* m);
 
 void sort_cols_by_min_element(matrix* m);
 
+matrix mul_matrices(matrix m1, matrix m2);
+
+void get_square_of_matrix_if_symmetric(matrix* m);
+
 #endif //CODE_ALGORITHMS_H
