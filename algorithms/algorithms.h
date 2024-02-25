2f39c647563c2c7859aec594d2a807c0216d9d16
3RBNK <dawidenkokirill@gmail.com>
2024-02-25 12:52:04 +0300

    add count non descinding rows matrices function

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -46,4 +46,7 @@ int get_n_special_element(matrix m);
 
 void swap_penultimate_row(matrix* m, int n);
 
+
+int count_non_descending_rows_matrices(matrix ms[], int n_matrix);
+
 #endif //CODE_ALGORITHMS_H
